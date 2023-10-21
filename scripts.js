9d43d67158813fd063450ae9938ebb9a9e494f6d
at-dulnuan <at.dulnuan@gmail.com>
2023-10-21 10:22:49 +0400

    First commit

---
+++ scripts.js
@@ -0,0 +1,16 @@
+AOS.init();
+
+const burgerLines = $('#burgerMenu .line')
+
+$("#burgerMenu").on("click", function () {
+    $("#burgerMenu").toggleClass("active");
+    $(".navigation").toggleClass("is-active");
+    $("#navMenu").toggleClass("white");
+    for (var i = 0; i < 3; i++) {
+        burgerLines[i].style.backgroundColor = "#323031";
+    }
+})
+
+$("#works").on("click", function(){
+    $(this).toggleClass("is-active")
+})
